5ce8bf0dcc1ec4c23edeb4d7ad7bf169d0302ccb
Gabriel Petersson <gabriielpetersson@gmail.com>
2021-01-19 15:12:39 +0200

    fix word gen

--- server/src/word-generator.ts
+++ server/src/word-generator.ts
@@ -22,10 +22,18 @@ export class WordGenerator {
   }
   private generate = () => words[Math.floor(Math.random() * words.length)]
   newWord = () => {
+    let count = 0
     let newWord = this.generate()
     while (this.usedWords.includes(newWord)) {
+      count++
       newWord = this.generate()
       if (this.usedWords.length === words.length) this.usedWords = []
+      if (count > 100) {
+        console.log("server error: infinite new word loop")
+        this.usedWords = []
+        newWord = this.generate()
+        break
+      }
     }
     this.usedWords.push(newWord)
     return newWord
